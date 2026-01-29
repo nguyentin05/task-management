e19b76d9521cd88b82b764ba95dd8c6851f17a54
Nguyen Trong Tin <tin.nguyen.cs05@gmail.com>
2026-01-29 13:58:06 +0700

    build(project): setup backend by spring modulith

---
+++ backend/src/test/java/com/ntt/taskmanagement/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntt.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
