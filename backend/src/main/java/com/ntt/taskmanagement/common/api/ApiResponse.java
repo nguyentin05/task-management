929ce17d34ffc2c28540d7fb6eca275f4137691f
Nguyen Trong Tin <tin.nguyen.cs05@gmail.com>
2026-01-30 11:45:42 +0700

    feat(be): update api response

---
+++ backend/src/main/java/com/ntt/taskmanagement/common/api/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.ntt.taskmanagement.common.api;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+    @Builder.Default
+    int code = 1000;
+
+    String message;
+    T result;
+}
