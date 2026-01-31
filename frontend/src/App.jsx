fd8e64a3dbab01aa5b7a9d528b19fdd0255c5758
aturanium <tatstar2k5@gmail.com>
2026-01-31 21:55:04 +0700

    feat(fe): add frontend project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import { MyUserContext } from "./configs/MyContexts";
+import { useReducer } from "react";
+import { UserReducer } from "./reducers/MyUserReducer";
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  const [user, dispatch] = useReducer(UserReducer, null);
+
+  return (
+    <MyUserContext.Provider value={[user, dispatch]}>
+      <BrowserRouter>
+        <Header />
+
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Container>
+
+        <Footer />
+      </BrowserRouter>
+    </MyUserContext.Provider>
+  );
+};
+
+export default App;
---
+++ frontend/src/reducers/MyUserReducer.jsx
@@ -0,0 +1,12 @@
+import cookie from "react-cookies";
+
+export const UserReducer = (current, action) => {
+  switch (action.type) {
+    case "login":
+      return action.payload;
+    case "logout":
+      cookie.remove("token");
+      return null;
+  }
+  return current;
+};
