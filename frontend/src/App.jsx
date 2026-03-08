532a48f2474e1268b371acd3c5eed7ec6330b656
aturanium <tatstar2k5@gmail.com>
2026-03-08 11:27:54 +0700

    feat(fe): admin curd UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/Home";
@@ -6,13 +6,29 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import { MyUserContext } from "./configs/MyContexts";
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
 import { UserReducer } from "./reducers/MyUserReducer";
 import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import cookie from "react-cookies";
+import Admin from "./components/Admin";
+import Permissions from "./components/admin/Permissions";
+import Roles from "./components/admin/Roles";
+import Users from "./components/admin/Users";
+import Profiles from "./components/admin/Profiles";
 
 const App = () => {
   const [user, dispatch] = useReducer(UserReducer, null);
+
+  useEffect(() => {
+    const token = cookie.load("token");
+
+    if (token) {
+      dispatch({
+        type: "login",
+      });
+    }
+  });
 
   return (
     <MyUserContext.Provider value={[user, dispatch]}>
@@ -24,7 +40,12 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/me" element={<Profile />} />
+            <Route path="/profiles/me" element={<Profile />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/admin/permissions" element={<Permissions />} />
+            <Route path="/admin/roles" element={<Roles />} />
+            <Route path="/admin/users" element={<Users />} />
+            <Route path="/admin/profiles" element={<Profiles />} />
           </Routes>
         </Container>
 
--- frontend/src/configs/Apis.jsx
+++ frontend/src/configs/Apis.jsx
@@ -4,14 +4,21 @@ import cookie from "react-cookies";
 const BASE_URL = "http://localhost:8080/";
 
 export const endpoints = {
-  register: "/auth/register",
-  login: "/auth/login",
+  register: "/auth/users",
+  login: "/auth/token",
   "refresh-token": "/auth/refresh",
   logout: "/auth/logout",
   "forget-password": "/auth/forgot-password",
   "reset-password": "/auth/reset-password",
-  me: "users/me",
-  "change-password": "users/me/change-password",
+  me: "/users/me",
+  "change-password": "/users/me/change-password",
+  "profile-me": "/profiles/me",
+  "update-profile": "/profiles/me",
+  "update-avatar": "/profiles/me/avatar",
+  permissions: "/auth/permissions",
+  roles: "/auth/roles",
+  users: "/auth/users",
+  profiles: "/profiles",
 };
 
 export const authApis = () => {
