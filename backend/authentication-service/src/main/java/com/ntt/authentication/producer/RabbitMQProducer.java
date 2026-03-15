98cd25d2a60f8575b58803a67fa8d3b511bf7782
Nguyen Trong Tin <tin.nguyen.cs05@gmail.com>
2026-03-15 22:45:31 +0700

    fix(be): fix message broker bug

--- backend/authentication-service/src/main/java/com/ntt/authentication/producer/RabbitMQProducer.java
+++ backend/authentication-service/src/main/java/com/ntt/authentication/producer/RabbitMQProducer.java
@@ -1,18 +1,27 @@
 package com.ntt.authentication.producer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 import com.ntt.authentication.configuration.RabbitMQConfig;
 
 import lombok.AllArgsConstructor;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class RabbitMQProducer {
     private final RabbitTemplate rabbitTemplate;
+    private final ObjectMapper objectMapper;
 
-    public void sendMessage(String routingKey, Object message) {
-        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, routingKey, message);
+    public void sendEvent(String routingKey, String payload) {
+        try {
+            Object event = objectMapper.readValue(payload, Object.class);
+            rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, routingKey, event);
+        } catch (Exception e) {
+            log.error("Bắn sự kiện thất bại: {}", e.getMessage(), e);
+        }
     }
 }
