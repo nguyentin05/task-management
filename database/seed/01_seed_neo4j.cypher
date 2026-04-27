2c207e53eb58b3a85047d94f8ea12d55954498fa
Nguyen Trong Tin <tin.nguyen.cs05@gmail.com>
2026-04-27 17:01:29 +0700

    chore(db): update seed data for auth, task and profile service

--- database/seed/01_seed_neo4j.cypher
+++ database/seed/01_seed_neo4j.cypher
@@ -1,12 +1,16 @@
-MERGE (a:Profile {userId: 'alice-123'})
-  ON CREATE SET a.firstName = 'Alice',
+MERGE (a:Profile {userId: '89a54de8-6090-4248-ba8b-7a8399b918c8'})
+ON CREATE SET
+  a.id = 'a1988cbd-5507-4b55-8fd5-659e627729d0',
+  a.firstName = 'Alice',
   a.lastName = 'Nguyen',
   a.dob = date('1999-01-15'),
   a.phone = '0901234567',
   a.avatar = 'https://res.cloudinary.com/dam6k8ezg/image/upload/v1775977979/k1rfh14faekvrhblbqtb.jpg';
 
-MERGE (b:Profile {userId: 'bob-456'})
-  ON CREATE SET b.firstName = 'Bob',
+MERGE (b:Profile {userId: 'aaf5d938-8871-41fd-ba32-8c87218d50e0'})
+ON CREATE SET
+  b.id = 'afd7f10a-086e-4ca6-9686-a02bae33aa3f',
+  b.firstName = 'Bob',
   b.lastName = 'Tran',
   b.dob = date('2000-05-20'),
   b.phone = '0907654321',
--- database/seed/03_seed_postgres.sql
+++ database/seed/03_seed_postgres.sql
@@ -1,49 +1,60 @@
-SET
-search_path TO auth_schema;
+SET search_path TO auth_schema;
 
 INSERT INTO roles (name, description)
 VALUES ('ROLE_USER', 'Standard User'),
        ('ROLE_ADMIN', 'Administrator') ON CONFLICT DO NOTHING;
 
---mật khẩu măc định là 'Test1234!' nhé--
+-- Mật khẩu mặc định: 'Test1234!'
 INSERT INTO users (id, email, password)
-VALUES ('alice-123', 'alice@example.com', '$2a$12$R9h/cIPz0gi.URNNX3kh2OPST9/PgBkqquzi.Ss7KIUgO2t0jWMUW'),
-       ('bob-456', 'bob@example.com',
-        '$2a$12$R9h/cIPz0gi.URNNX3kh2OPST9/PgBkqquzi.Ss7KIUgO2t0jWMUW') ON CONFLICT DO NOTHING;
+VALUES ('89a54de8-6090-4248-ba8b-7a8399b918c8', 'alice@example.com', '$2a$10$iuNVN6rIBtJes/Ip.XlU8e0cJXiYtbNOKfOCsnuksD5z3.urIwCLK'),
+       ('aaf5d938-8871-41fd-ba32-8c87218d50e0', 'bob@example.com', '$2a$10$QJaX14.jhQRc8weGOEyOE.NDYnB7WonvHKBkmkPMi1f2nFB3sJvfm')
+ON CONFLICT DO NOTHING;
 
 INSERT INTO user_role (user_id, role_name)
-VALUES ('alice-123', 'ROLE_ADMIN'),
-       ('bob-456', 'ROLE_USER') ON CONFLICT DO NOTHING;
+VALUES ('89a54de8-6090-4248-ba8b-7a8399b918c8', 'ROLE_ADMIN'),
+       ('aaf5d938-8871-41fd-ba32-8c87218d50e0', 'ROLE_USER')
+ON CONFLICT DO NOTHING;
 
-SET
-search_path TO task_schema;
+SET search_path TO task_schema;
 
 INSERT INTO workspaces (id, user_id, name, description)
-VALUES ('workspace-alice', 'alice-123', 'Alice Workspace', 'Private workspace of Alice'),
-       ('workspace-bob', 'bob-456', 'Bob Workspace', 'Private workspace of Bob') ON CONFLICT DO NOTHING;
+VALUES ('2a49a7a0-9649-41a7-b57c-abcdc36d1d3d', '89a54de8-6090-4248-ba8b-7a8399b918c8', 'Alice Workspace', 'Private workspace of Alice'),
+       ('2a368eb9-bb9a-4f5c-9e60-517e46c369dd', 'aaf5d938-8871-41fd-ba32-8c87218d50e0', 'Bob Workspace', 'Private workspace of Bob')
+ON CONFLICT DO NOTHING;
 
 INSERT INTO projects (id, name, description, created_by, start_at, end_at)
-VALUES ('project-1', 'Capstone Project', 'Task Management System Capstone', 'alice-123', CURRENT_TIMESTAMP,
-        CURRENT_TIMESTAMP + INTERVAL '30 days') ON CONFLICT DO NOTHING;
+VALUES ('dcf76e70-ab22-406c-9cdd-dcb0d11d7847', 'Capstone Project', 'Task Management System Capstone',
+        '89a54de8-6090-4248-ba8b-7a8399b918c8', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '30 days')
+ON CONFLICT DO NOTHING;
 
 INSERT INTO workspace_project (project_id, workspace_id)
-VALUES ('project-1', 'workspace-alice') ON CONFLICT DO NOTHING;
+VALUES ('dcf76e70-ab22-406c-9cdd-dcb0d11d7847', '2a49a7a0-9649-41a7-b57c-abcdc36d1d3d')
+ON CONFLICT DO NOTHING;
 
 INSERT INTO project_member (id, project_id, user_id, role)
-VALUES ('pm-1', 'project-1', 'alice-123', 'PROJECT_MANAGER'),
-       ('pm-2', 'project-1', 'bob-456', 'MEMBER') ON CONFLICT DO NOTHING;
+VALUES ('09c01652-4b96-41ba-9a5d-e6e3dffea7d6', 'dcf76e70-ab22-406c-9cdd-dcb0d11d7847',
+        '89a54de8-6090-4248-ba8b-7a8399b918c8', 'PROJECT_MANAGER'),
+       ('4a583ce0-af73-48e9-beef-351b38387455', 'dcf76e70-ab22-406c-9cdd-dcb0d11d7847',
+        'aaf5d938-8871-41fd-ba32-8c87218d50e0', 'MEMBER')
+ON CONFLICT DO NOTHING;
 
 INSERT INTO columns (id, project_id, name, position, is_done_column)
-VALUES ('col-1', 'project-1', 'To Do', 100.0, FALSE),
-       ('col-2', 'project-1', 'In Progress', 200.0, FALSE),
-       ('col-3', 'project-1', 'Done', 300.0, TRUE) ON CONFLICT DO NOTHING;
+VALUES ('d93c2476-af82-4a4f-89d6-a1dfaea9844e', 'dcf76e70-ab22-406c-9cdd-dcb0d11d7847', 'To Do', 100.0, FALSE),
+       ('9cdf2fec-640a-46fd-b7b9-65867038c444', 'dcf76e70-ab22-406c-9cdd-dcb0d11d7847', 'In Progress', 200.0, FALSE),
+       ('3d30b5b5-692d-4fe1-94bf-318a9e28daee', 'dcf76e70-ab22-406c-9cdd-dcb0d11d7847', 'Done', 300.0, TRUE)
+ON CONFLICT DO NOTHING;
 
 INSERT INTO tasks (id, column_id, title, description, position, created_by, assignee_id, label)
-VALUES ('task-1', 'col-1', 'Cấu trúc thư mục Microservices', 'Sử dụng Spring Boot và Maven Multi-module', 100.0,
-        'alice-123', 'alice-123', 'BACKEND'),
-       ('task-2', 'col-1', 'Thiết kế Database (ERD)', 'Sắp xếp schema cho Postgre, Neo4j, MongoDB', 200.0, 'alice-123',
-        'bob-456', 'DATABASE'),
-       ('task-3', 'col-2', 'Implement API Gateway', 'Sử dụng Spring Cloud Gateway + WebFlux Filter', 100.0, 'alice-123',
-        'alice-123', 'GATEWAY'),
-       ('task-4', 'col-3', 'Setup CI/CD Github Actions', 'Cấu hình build tự động Docker image', 100.0, 'alice-123',
-        'bob-456', 'DEVOPS') ON CONFLICT DO NOTHING;
+VALUES ('8a3f5bca-5195-4af6-bf2a-a0adc482452a', 'd93c2476-af82-4a4f-89d6-a1dfaea9844e',
+        'Cấu trúc thư mục Microservices', 'Sử dụng Spring Boot và Maven Multi-module', 100.0,
+        '89a54de8-6090-4248-ba8b-7a8399b918c8', '89a54de8-6090-4248-ba8b-7a8399b918c8', 'BACKEND'),
+       ('4afbe345-54f8-4751-844d-2b84c77a78ed', 'd93c2476-af82-4a4f-89d6-a1dfaea9844e',
+        'Thiết kế Database (ERD)', 'Sắp xếp schema cho Postgre, Neo4j, MongoDB', 200.0,
+        '89a54de8-6090-4248-ba8b-7a8399b918c8', 'aaf5d938-8871-41fd-ba32-8c87218d50e0', 'DATABASE'),
+       ('950ebe06-499e-4d49-b9c8-ea095d753010', '9cdf2fec-640a-46fd-b7b9-65867038c444',
+        'Implement API Gateway', 'Sử dụng Spring Cloud Gateway + WebFlux Filter', 100.0,
+        '89a54de8-6090-4248-ba8b-7a8399b918c8', '89a54de8-6090-4248-ba8b-7a8399b918c8', 'GATEWAY'),
+       ('89998628-ae9a-4890-b32c-535e785e92bb', '3d30b5b5-692d-4fe1-94bf-318a9e28daee',
+        'Setup CI/CD Github Actions', 'Cấu hình build tự động Docker image', 100.0,
+        '89a54de8-6090-4248-ba8b-7a8399b918c8', 'aaf5d938-8871-41fd-ba32-8c87218d50e0', 'DEVOPS')
+ON CONFLICT DO NOTHING;
